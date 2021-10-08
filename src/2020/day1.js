b9cfc24e17fba5c607a15f1298f213448cbcbf74
mariofdezzz <47794126+mariofdezzz@users.noreply.github.com>
2021-10-08 18:06:04 +0100

    Lint day1.js

--- src/2020/day1.js
+++ src/2020/day1.js
@@ -1,35 +1,37 @@
 const target = 2020
 const table = (input) =>
-    input.reduce((acc, item) => {
-        acc[item] = item
-        return acc
-    }, {})
+  input.reduce((acc, item) => {
+    acc[item] = item
+    return acc
+  }, {})
 
 // Cost O(n)
 function part1 (input) {
-  let dict = table(input)
+  const dict = table(input)
 
-  for (const el of input)
-      if (dict.hasOwnProperty(target - el))
-          if (target - el != el) return (target - el) * el
+  for (const el of input) {
+    if (dict[(target - el) + ''] !== undefined) { if (target - el !== el) return (target - el) * el }
+  }
 
-  return "Not found"
+  return 'Not found'
 }
 
 function part2 (input) {
-  let dict = table(input)
+  const dict = table(input)
 
-  for (const elA of input)
-      for (const elB of input)
-          if (dict.hasOwnProperty(target - elA - elB))
-              if (
-                  target - elA - elB != elA &&
-                  target - elA - elB != elB &&
-                  elA != elB
-              )
-                  return (target - elA - elB) * elA * elB
+  for (const elA of input) {
+    for (const elB of input) {
+      if (dict[(target - elA - elB) + ''] !== undefined) {
+        if (
+          target - elA - elB !== elA &&
+                  target - elA - elB !== elB &&
+                  elA !== elB
+        ) { return (target - elA - elB) * elA * elB }
+      }
+    }
+  }
 
-  return "Not found"
+  return 'Not found'
 }
 
 module.exports = {
